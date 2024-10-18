90e6fb1db46aad418725079c2d9a03e0669baba7
Aguedo Valdes <aguedo@aslanta.com>
2024-10-18 07:25:33 -0500

    Setup navigation layout.

---
+++ source/Aslanta.Billing.Web/Components/Layout/Navigation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Aslanta.Billing.Web.Components.Layout
+{
+    public class NavigationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
