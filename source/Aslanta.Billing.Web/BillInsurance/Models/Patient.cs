78b945998cfdb056029ea451d5f6ace6a451821e
Aguedo Valdes <aguedo@aslanta.com>
2024-10-19 08:50:09 -0500

    Add Patient model.

---
+++ source/Aslanta.Billing.Web/BillInsurance/Models/Patient.cs
@@ -0,0 +1,9 @@
+namespace Aslanta.Billing.Web.BillInsurance.Models;
+
+public class Patient
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+}
