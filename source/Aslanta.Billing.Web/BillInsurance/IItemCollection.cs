97b9565a140b00c562014f2f5fd41b8158c353e0
Aguedo Valdes <aguedo@aslanta.com>
2024-10-21 08:43:48 -0500

    Add more methods to the ItemCollection interface.

--- source/Aslanta.Billing.Web/BillInsurance/IItemCollection.cs
+++ source/Aslanta.Billing.Web/BillInsurance/IItemCollection.cs
@@ -4,11 +4,15 @@ public interface IItemCollection<T>
 {
     IReadOnlyCollection<T> Items { get; }
     IReadOnlyCollection<T> SelectedItems { get; }
-    bool IsSelected(T item);
     void Add(params T[] items);
     void Add(IEnumerable<T> items);
     void Remove(params T[] items);
     void Remove(IEnumerable<T> items);
     void RemoveSelected();
     void Clear();
+    bool IsSelected(T item);
+    void Select(T item);
+    void Unselect(T item);
+    void SelectAll();
+    void UnselectAll();
 }
