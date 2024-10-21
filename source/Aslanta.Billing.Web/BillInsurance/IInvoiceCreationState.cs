b5326d0b0ca1e97ec07ab94c194b6fa6bed8b954
Aguedo Valdes <aguedo@aslanta.com>
2024-10-21 08:56:00 -0500

    Add selection methods to the InvoiceCreation interface.

--- source/Aslanta.Billing.Web/BillInsurance/IInvoiceCreationState.cs
+++ source/Aslanta.Billing.Web/BillInsurance/IInvoiceCreationState.cs
@@ -4,9 +4,15 @@ namespace Aslanta.Billing.Web.BillInsurance;
 
 public interface IInvoiceCreationState
 {
+    IReadOnlyCollection<Order> SelectedOrders { get; }
     void AddOrders(params Order[] orders);
     void AddOrders(IEnumerable<Order> orders);
     void RemoveOrders(params Order[] orders);
     void RemoveOrders(IEnumerable<Order> orders);
     void RemoveSelectedOrders();
+    void SelectOrder(Order order);
+    void UnselectOrder(Order order);
+    void SelectAllOrders();
+    void UnselectAllOrders();
+    bool IsOrderSelected(Order order);
 }
